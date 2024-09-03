b5a02539a8319be512437b3b51f630b881479101
Bruna Holanda <78491263+Bruhollanda@users.noreply.github.com>
2024-09-03 13:54:35 -0300

    Update MovelController.java

--- soa_rest/src/main/java/com/loja_moveis/controller/MovelController.java
+++ soa_rest/src/main/java/com/loja_moveis/controller/MovelController.java
@@ -14,34 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/produto")
 public class ProdutoController {
-    private final ProdutoService service;
+    private final MovelService service;
     @GetMapping("/list")
-    public ResponseEntity<Page<ProdutoResponse>> listar(PaginatedRequest paginatedRequest) {
+    public ResponseEntity<Page<MovelResponse>> listar(PaginatedRequest paginatedRequest) {
         return ResponseEntity.ok(service.listar(paginatedRequest));
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<ProdutoResponse> cadastrar(@RequestBody ProdutoRequest request) {
-        return ResponseEntity.ok(new ProdutoResponse(service.cadastrar(request)));
+    public ResponseEntity<MovelResponse> cadastrar(@RequestBody MovelRequest request) {
+        return ResponseEntity.ok(new MovelResponse(service.cadastrar(request)));
     }
 
 //    @GetMapping("/{id}")
-//    public ResponseEntity<FornecedorResponse> getById(@PathVariable Long id) {
-//        return ResponseEntity.ok(new FornecedorResponse(service.getById(id)));
-//    }
-//
-//    @PostMapping("/cadastrar")
-//    public ResponseEntity<FornecedorResponse> cadastrar(@RequestBody FornecedorRequest request) {
-//        return ResponseEntity.ok(new FornecedorResponse(service.cadastrar(request)));
+//    public ResponseEntity<MovelrResponse> getById(@PathVariable Long id) {
+//        return ResponseEntity.ok(new MovelResponse(service.getById(id)));
 //    }
 //
 //    @PostMapping("/update/{id}")
-//    public ResponseEntity<FornecedorResponse> update(@RequestBody FornecedorRequest request, @PathVariable Long id) {
-//        return ResponseEntity.ok(new FornecedorResponse(service.update(request, id)));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleterById(@PathVariable Long id) {
-//        return ResponseEntity.noContent().build();
+//    public ResponseEntity<MovelResponse> update(@RequestBody MovelRequest request, @PathVariable Long id) {
+//        return ResponseEntity.ok(new MovelResponse(service.update(request, id)));
 //    }
 }
