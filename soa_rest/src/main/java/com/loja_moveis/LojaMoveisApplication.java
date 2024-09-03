768fdd73a10d80876ddf9d7ec728db1fec1b7f0c
Bruna <brunaholandacunha7@gmail.com>
2024-09-03 09:51:04 -0300

    Restarting

---
+++ soa_rest/src/main/java/com/loja_moveis/LojaMoveisApplication.java
@@ -0,0 +1,13 @@
+package com.loja_moveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaMoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaMoveisApplication.class, args);
+	}
+
+}
---
+++ soa_rest/src/test/java/com/loja_moveis/LojaMoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja_moveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaMoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
